e5b3fc9ef84d53cb1462f35fdbca559e0814b038
Abdellah DAMRI <damriabdellah@gmail.com>
2024-02-04 14:18:41 +0100

    init project

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, {
+  IRouterMatcher,
+  Request,
+  Response,
+  Application,
+} from "express";
+import dotenv from "dotenv";
+import { connectDB } from "@/config";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 8000;
+
+connectDB();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    msg: "API Running...",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
